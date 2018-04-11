eebbb8c678e157250f156f3e9d49e92d2694317a
BrunoMelo <brmelo86@hotmail.com>
2018-04-11 10:55:06 -0300

    Initial commit

---
+++ features/step_definitions/test_steps.rb
@@ -0,0 +1,11 @@
+Dado("que estou na tela inicial") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+Dado("toco no botao {string}") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+Entao("sou direcionado para tela {int}") do |int|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
