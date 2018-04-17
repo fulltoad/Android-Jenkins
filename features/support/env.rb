8dd4bf645700f3dcf570ebc36c00103608fae942
Bruno Melo <brmelo86@hotmail.com>
2018-04-17 13:43:41 -0300

    Ajust to work with Jenkins

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,7 +1,7 @@
 require 'appium_lib'
 require 'pry'
 
-caps_path = File.join(File.dirname(__FILE__), '..', '..','appium.txt')
+caps_path = File.join(File.dirname(__FILE__), '..', '..', 'caps', ENV['QA'], "#{ENV['PLATFORM']}.txt")
 
 caps = Appium.load_appium_txt file: caps_path, verbose: true
 
@@ -13,9 +13,9 @@ Before do
 end
 
 After do |scenario |
-    file_name = "screenshot_#{}.png"
-    $driver.screenshot(File.join("/Users/brunomelo/test/cucumber/screenshot", file _file_name)) if test
-    scenario.failed?
+#    file_name = "screenshot_#{}.png"
+#    $driver.screenshot(File.join("/Users/brunomelo/test/cucumber/screenshot", file _file_name)) if test
+#    scenario.failed?
     $driver.driver_quit
 end
 
